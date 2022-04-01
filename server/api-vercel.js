6b234504da3532504e2a83866dc88ca64300ed97
LeRoidelaRef <67747437+LeRoidelaRef@users.noreply.github.com>
2022-04-01 16:37:11 +0200

    Update api-vercel.js

--- server/api-vercel.js
+++ server/api-vercel.js
@@ -1,69 +1,74 @@
-const clientPromise = require('./mongodb-client');
-const ObjectId = require("mongodb").ObjectID;
 const express = require('express');
+const MongoClient = require('mongodb');
+const products = require('./products.json')
+const MONGODB_DB_NAME= "clearfashion"
+const client = require('./mongodb-client.js')
 const app = express();
 
 const { calculateLimitAndOffset, paginate } = require('paginate-info');
 
-const  DATABASE_NAME = "clear-fashion";
 
-app.get('/', async (request, response) => {
-    const client = await clientPromise;
-    console.log("Connected to `" + client.db().databaseName + "` database !");
-    response.send({'ack': true, 'dbConnection' : true, 'dbName': client.db().databaseName});
+app.get('/', (request, response) => {
+  response.send({'test': true});
 });
 
-app.get('/products/search', async (request, response) => {
-    const client = await clientPromise;
-    const collection = await client.db(DATABASE_NAME).collection("products");
+app.get('/products/search', (request, response) => {
 
-    const filters = {};
-    var brand, price;
-    const size = parseInt(request.query.size, 10) || 12;
-    const page = parseInt(request.query.page, 10) || 1;
+  var filter ={};
+  var limit = parseInt(request.query.limit);
+  var brand;
+  var price;
+  const count = parseInt(request.query.size, 10) || 12;
+  const page = parseInt(request.query.page, 10) || 1;
+
+  const clien = await client
+  const collection = clien.db(MONGODB_DB_NAME).collection("products");
   
-    if(request.query.brand !== undefined){
-      brand = request.query.brand,
-      filters["brand"] = brand;
-    }
-    if(request.query.price !== undefined){
-      price = parseInt(request.query.price, 10);
-      filters["price"] = {$lte: price};
-    }
+  if (request.query.brand != undefined){
+    filter['brand']=request.query.brand;
+  }
 
-    const { limit, offset } = calculateLimitAndOffset(page, size);
-    const count = await collection.count();
+  if(request.query.price != undefined){
+    filter["price"]=request.query.price;
+  }
 
-    await collection.find(filters).sort({ price: 1 }).skip(offset).limit(limit).toArray((error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send({"data": {"result": result, "meta": paginate(page, count, result, limit)}, "success": true});
-      });
+  const {limit,offset} = calculateLimitAndOffset(page,size);
+
+  collection.find(filter).skip(offset).limit(limit).toArray((error,result)=>{
+    if (error){
+      return response.status(500).send(error)
+    }
+    response.send({"success":true,"data":{"result":result,"meta":paginate(page, collection.count(), result, limit)}})
+  });
 });
 
-app.get('/products/:id', async (request, response) => {
-    const client = await clientPromise;
-    const collection = await client.db(DATABASE_NAME).collection("products");
 
-    await collection.findOne({ "_id": new ObjectId(request.params.id)}, (error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+app.get('/products/:id', (request, response) => {
+
+  const clien = await client
+  const collection = clien.db(MONGODB_DB_NAME).collection("products");
+
+
+  collection.findOne({ "_id": request.params.id}, (error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+      response.send(result);
   });
+});
 
-app.get('/products', async (request, response) => {
-    const client = await clientPromise;
-    const collection = await client.db(DATABASE_NAME).collection("products");
+app.get('/products/:', (request, response) => {
 
-    await collection.find({ }).toArray((error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+  const clien = await client
+  const collection = clien.db(MONGODB_DB_NAME).collection("products");
+
+
+  collection.find({ }).toArray((error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+      response.send(result);
+  });
 });
 
 module.exports = app;
